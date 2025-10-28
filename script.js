c2ab91f2c8dcac53ee0204b3593fa6b1498ee3a0
mldnaacl-lab <mldnaacl@gmail.com>
2025-10-28 11:25:35 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1. pilih tombol berdasarkan ID-nya
+const tombolSapa = document.getElamanByld('sapaButton")
+// 2. tambahkan 'event listener' untuk aksi 'click'
+tombolsapa.addEventlistener('clikk',  function(){
+  // 3. jalankan kode ini ketika tombol di-klik
+  alert('helo! Terima kasih sudah berkunjung!');
+});
+                                          
+                                
